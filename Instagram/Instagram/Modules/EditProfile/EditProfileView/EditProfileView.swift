48027418ee5f9f1277dc9f1120f6f253d2b2ea4a
alecsmirnov <alecsmirnov93@gmail.com>
2021-03-07 14:52:08 +0700

    Added MaterialTextField

--- Instagram/Instagram/Modules/EditProfile/EditProfileView/EditProfileView.swift
+++ Instagram/Instagram/Modules/EditProfile/EditProfileView/EditProfileView.swift
@@ -8,18 +8,228 @@
 import UIKit
 
 final class EditProfileView: UIView {
+    // MARK: Properties
+    
+    var keyboardAppearanceListener: KeyboardAppearanceListener?
+    
+    // MARK: Subviews
+    
+    private let scrollView = UIScrollView()
+    private let screenView = UIView()
+    
+    private let profileImageButton = UIButton(type: .system)
+    private let changeProfileImageButton = UIButton(type: .system)
+    
+    private let nameTextField = MaterialTextField()
+    private let usernameTextField = MaterialTextField()
+    private let websiteTextField = MaterialTextField()
+    private let bioTextField = MaterialTextField()
+    
     // MARK: Initialization
     
     init() {
         super.init(frame: .zero)
         
-        backgroundColor = .cyan
+        setupAppearance()
+        setupLayout()
         
-        //setupAppearance()
-        //setupLayout()
+        keyboardAppearanceListener = KeyboardAppearanceListener(delegate: self)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Appearance
+
+private extension EditProfileView {
+    func setupAppearance() {
+        backgroundColor = .systemBackground
+        
+        setupScrollViewAppearance()
+        setupProfileImageButtonAppearance()
+        setupChangeProfileImageButtonAppearance()
+        setupNameTextFieldAppearance()
+        setupUsernameTextFieldAppearance()
+        setupWebsiteTextFieldAppearance()
+        setupBioTextFieldAppearance()
+    }
+    
+    func setupScrollViewAppearance() {
+        scrollView.delaysContentTouches = false
+    }
+    
+    func setupProfileImageButtonAppearance() {
+        profileImageButton.setImage(LoginRegistrationConstants.Images.profile, for: .normal)
+        profileImageButton.tintColor = LoginRegistrationConstants.Colors.profileImageButtonTint
+        profileImageButton.layer.cornerRadius = 80 / 2
+        profileImageButton.layer.masksToBounds = true
+        profileImageButton.layer.borderColor = LoginRegistrationConstants.Colors.profileImageButtonBorder.cgColor
+        profileImageButton.layer.borderWidth = LoginRegistrationConstants.Metrics.profileImageButtonBorderWidth
+    }
+    
+    func setupChangeProfileImageButtonAppearance() {
+        changeProfileImageButton.setTitle("Change profile photo", for: .normal)
+    }
+    
+    func setupNameTextFieldAppearance() {
+        nameTextField.placeholder = "Name"
+        nameTextField.font = .systemFont(ofSize: 15)
+    }
+    
+    func setupUsernameTextFieldAppearance() {
+        usernameTextField.placeholder = "Username"
+        usernameTextField.font = .systemFont(ofSize: 15)
+    }
+    
+    func setupWebsiteTextFieldAppearance() {
+        websiteTextField.placeholder = "Website"
+        websiteTextField.font = .systemFont(ofSize: 15)
+    }
+    
+    func setupBioTextFieldAppearance() {
+        bioTextField.placeholder = "Bio"
+        bioTextField.font = .systemFont(ofSize: 15)
+    }
+}
+
+// MARK: - Layout
+
+private extension EditProfileView {
+    func setupLayout() {
+        setupSubviews()
+        
+        setupScrollViewLayout()
+        setupScreenViewLayout()
+        setupProfileImageButtonLayout()
+        setupChangeProfileImageButtonLayout()
+        setupNameTextFieldLayout()
+        setupUsernameTextFieldLayout()
+        setupWebsiteTextFieldLayout()
+        setupBioTextFieldLayout()
+    }
+    
+    func setupSubviews() {
+        addSubview(scrollView)
+        
+        scrollView.addSubview(screenView)
+        
+        screenView.addSubview(profileImageButton)
+        screenView.addSubview(changeProfileImageButton)
+        scrollView.addSubview(nameTextField)
+        scrollView.addSubview(usernameTextField)
+        scrollView.addSubview(websiteTextField)
+        scrollView.addSubview(bioTextField)
+    }
+    
+    func setupScrollViewLayout() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+        ])
+    }
+    
+    func setupScreenViewLayout() {
+        screenView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            screenView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            screenView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            screenView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            screenView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            screenView.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor),
+        ])
+    }
+    
+    func setupProfileImageButtonLayout() {
+        profileImageButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            profileImageButton.topAnchor.constraint(equalTo: screenView.topAnchor, constant: 50),
+            profileImageButton.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            profileImageButton.heightAnchor.constraint(equalToConstant: 80),
+            profileImageButton.widthAnchor.constraint(equalToConstant: 80),
+        ])
+    }
+    
+    func setupChangeProfileImageButtonLayout() {
+        changeProfileImageButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            changeProfileImageButton.topAnchor.constraint(equalTo: profileImageButton.bottomAnchor, constant: 8),
+            changeProfileImageButton.centerXAnchor.constraint(equalTo: profileImageButton.centerXAnchor),
+        ])
+    }
+    
+    func setupNameTextFieldLayout() {
+        nameTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nameTextField.topAnchor.constraint(equalTo: changeProfileImageButton.bottomAnchor, constant: 16),
+            nameTextField.leadingAnchor.constraint(equalTo: screenView.leadingAnchor, constant: 16),
+            nameTextField.trailingAnchor.constraint(equalTo: screenView.trailingAnchor, constant: -16),
+        ])
+    }
+    
+    func setupUsernameTextFieldLayout() {
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 16),
+            usernameTextField.leadingAnchor.constraint(equalTo: screenView.leadingAnchor, constant: 16),
+            usernameTextField.trailingAnchor.constraint(equalTo: screenView.trailingAnchor, constant: -16),
+        ])
+    }
+    
+    func setupWebsiteTextFieldLayout() {
+        websiteTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            websiteTextField.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 16),
+            websiteTextField.leadingAnchor.constraint(equalTo: screenView.leadingAnchor, constant: 16),
+            websiteTextField.trailingAnchor.constraint(equalTo: screenView.trailingAnchor, constant: -16),
+        ])
+    }
+    
+    func setupBioTextFieldLayout() {
+        bioTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            bioTextField.topAnchor.constraint(equalTo: websiteTextField.bottomAnchor, constant: 16),
+            bioTextField.leadingAnchor.constraint(equalTo: screenView.leadingAnchor, constant: 16),
+            bioTextField.trailingAnchor.constraint(equalTo: screenView.trailingAnchor, constant: -16),
+        ])
+    }
+}
+
+// MARK: - KeyboardAppearanceListenerDelegate
+
+extension EditProfileView: KeyboardAppearanceListenerDelegate {
+    func keyboardAppearanceListener(
+        _ listener: KeyboardAppearanceListener,
+        keyboardWillShowWith notification: Notification
+    ) {
+        guard
+            let userInfo = notification.userInfo,
+            let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+        else {
+            return
+        }
+        
+        scrollView.contentInset.bottom = keyboardSize.cgRectValue.height
+        scrollView.verticalScrollIndicatorInsets.bottom = scrollView.contentInset.bottom
+    }
+    
+    func keyboardAppearanceListener(
+        _ listener: KeyboardAppearanceListener,
+        keyboardWillHideWith notification: Notification
+    ) {
+        scrollView.contentInset.bottom = 0
+        scrollView.verticalScrollIndicatorInsets.bottom = scrollView.contentInset.bottom
     }
 }
---
+++ Instagram/Instagram/Utilities/Views/MaterialTextField.swift
@@ -0,0 +1,164 @@
+//
+//  MaterialTextField.swift
+//  Instagram
+//
+//  Created by Admin on 07.03.2021.
+//
+
+import UIKit
+
+final class MaterialTextField: UITextField {
+    // MARK: Properties
+    
+    override var placeholder: String? {
+        didSet {
+            placeholderLabel.text = placeholder
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let contentHeight = textInsets.top + textHeight + textInsets.bottom
+        let contentSize = CGSize(width: bounds.width, height: contentHeight)
+        
+        return contentSize
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: textInsets)
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: textInsets)
+    }
+
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return .zero
+    }
+    
+    private var textInsets: UIEdgeInsets {
+        let textHeight = Metrics.verticalSpace + fontSize
+        let textInsets = UIEdgeInsets(top: textHeight, left: 0, bottom: Metrics.verticalSpace, right: 0)
+        
+        return textInsets
+    }
+    
+    private var fontSize: CGFloat {
+        return font?.pointSize ?? 0
+    }
+    
+    private var textHeight: CGFloat {
+        return font?.lineHeight ?? 0
+    }
+    
+    private var isEmpty: Bool {
+        return text?.isEmpty ?? true
+    }
+    
+    // MARK: Constants
+    
+    private enum Metrics {
+        static let verticalSpace: CGFloat = 8
+        static let placeholderLabelScale: CGFloat = 0.85
+        static let bottomBorderHeight: CGFloat = 1
+    }
+    
+    private enum Colors {
+        static let active = UIColor.black
+        static let inactive = UIColor.lightGray
+    }
+    
+    private enum Constants {
+        static let animationDuration: TimeInterval = 0.25
+    }
+    
+    // MARK: Subviews
+    
+    private let placeholderLabel = UILabel()
+    private let bottomBorder = UIView()
+    
+    // MARK: Lifecycle
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        setupPlaceholderLabelLayout()
+    }
+    
+    // MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupAppearance()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Appearance
+
+private extension MaterialTextField {
+    func setupAppearance() {
+        placeholderLabel.textColor = Colors.inactive
+        bottomBorder.backgroundColor = Colors.inactive
+        
+        addAction(UIAction { [weak self] _ in
+            self?.updatePlaceholderLabel()
+            self?.updateBorderStyle()
+        }, for: .allEditingEvents)
+    }
+    
+    private func updatePlaceholderLabel() {
+        let scale = Metrics.placeholderLabelScale
+        
+        let offsetY = Metrics.verticalSpace + fontSize
+        let translationX = -placeholderLabel.bounds.width * (1 - scale) / 2
+        let translationY = -placeholderLabel.bounds.height * (1 - scale) / 2 - offsetY
+
+        let transform = CGAffineTransform(translationX: translationX, y: translationY).scaledBy(x: scale, y: scale)
+        
+        UIView.animate(withDuration: Constants.animationDuration) { [self] in
+            placeholderLabel.transform = (isFirstResponder || !isEmpty) ? transform : .identity
+        }
+    }
+    
+    func updateBorderStyle() {
+        UIView.animate(withDuration: Constants.animationDuration) { [self] in
+            bottomBorder.backgroundColor = isFirstResponder ? Colors.active : Colors.inactive
+        }
+    }
+}
+
+// MARK: - Layout
+
+private extension MaterialTextField {
+    func setupLayout() {
+        setupSubviews()
+        
+        setupBottomBorderLayout()
+    }
+    
+    func setupSubviews() {
+        addSubview(placeholderLabel)
+        addSubview(bottomBorder)
+    }
+    
+    func setupPlaceholderLabelLayout() {
+        placeholderLabel.transform = .identity
+        placeholderLabel.frame = bounds.inset(by: textInsets)
+    }
+    
+    func setupBottomBorderLayout() {
+        bottomBorder.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            bottomBorder.bottomAnchor.constraint(equalTo: bottomAnchor),
+            bottomBorder.leadingAnchor.constraint(equalTo: leadingAnchor),
+            bottomBorder.trailingAnchor.constraint(equalTo: trailingAnchor),
+            bottomBorder.heightAnchor.constraint(equalToConstant: Metrics.bottomBorderHeight),
+        ])
+    }
+}
